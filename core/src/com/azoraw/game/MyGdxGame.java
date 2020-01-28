74a7be9cfbeb1542666b0ba119feafd34e041495
a.zoraw <a.zoraw@partner.samsung.com>
2020-01-28 15:58:24 +0100

    draw simple rectangle grid

--- core/src/com/azoraw/game/MyGdxGame.java
+++ core/src/com/azoraw/game/MyGdxGame.java
@@ -1,33 +1,37 @@
 package com.azoraw.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    ShapeRenderer shapeRenderer;
+    int width = 50;
+    int height = 50;
+    int space = 60;
+
+    @Override
+    public void create() {
+        shapeRenderer = new ShapeRenderer();
+    }
+
+    @Override
+    public void render() {
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.RED);
+
+        for (int x = 0; x < 5; x++) {
+            for (int y = 0; y < 5; y++) {
+                shapeRenderer.rect(x * space, y * space, width, height);
+            }
+        }
+        shapeRenderer.end();
+    }
+
+
+    @Override
+    public void dispose() {
+        shapeRenderer.dispose();
+    }
 }
